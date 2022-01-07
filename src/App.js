5391281198957f579743942b3311d25301f33c53
Jack Herrington <jherr@pobox.com>
2022-01-06 18:17:08 -0800

    Updates

--- src/App.js
+++ src/App.js
@@ -1,106 +1,156 @@
 import React from "react";
 
-import DataGrid, { RowDragging } from "devextreme-react/data-grid";
-import ArrayStore from "devextreme/data/array_store";
+import { Draggable, Droppable } from "react-drag-and-drop";
 
 import { objects1, objects2 } from "./data.js";
 
-const columns = ["Color"];
-const draggingGroupName = "appointmentsGroup";
-
-const w = 20;
-const h = 20;
-
-const store = new ArrayStore({
-  data: objects2,
-  key: "ID"
-});
-
-const MyCanvas = () => {
+const MyCanvas = ({ list }) => {
   return (
-    <div width={300} height={300}>
-      {objects1.map(({ Color, ID, Position }) => (
-        <div
+    <div
+      width={300}
+      height={300}
+      style={{
+        position: "relative",
+      }}
+    >
+      {list.map(({ Color, ID, Position }) => (
+        <Draggable
+          type="appointment"
+          data={ID}
           key={ID}
           style={{
             position: "absolute",
             left: Position.x,
             top: Position.y,
-            width: w,
-            height: h,
-            background: Color
+            width: 20,
+            height: 20,
+            background: Color,
+            borderRadius: "0.5em",
           }}
-        ></div>
+        ></Draggable>
+      ))}
+    </div>
+  );
+};
+
+const DraggableList = ({ list }) => {
+  return (
+    <div
+      style={{
+        fontSize: "1rem",
+      }}
+    >
+      <div
+        style={{
+          color: "gray",
+          marginLeft: 8,
+          paddingLeft: 5,
+        }}
+      >
+        Color
+      </div>
+      {list.map(({ Color, ID }) => (
+        <Draggable type="item" data={ID} key={ID}>
+          <div
+            style={{
+              marginTop: 4,
+              borderBottom: 0,
+              borderTop: 0,
+              borderRight: 0,
+              borderLeft: 8,
+              borderColor: "gray",
+              borderStyle: "solid",
+              paddingLeft: 5,
+            }}
+          >
+            {Color}
+          </div>
+        </Draggable>
       ))}
     </div>
   );
 };
 
 function App() {
-  const [list, setList] = React.useState(objects1);
+  const [list1, setList1] = React.useState(objects1);
+  const [list2, setList2] = React.useState(objects2);
 
-  const onAdd = React.useCallback(
-    (e) => {
-      const index = list.indexOf(e.fromData);
+  const onAppointmentDrop = (data) => {
+    const index = list1.findIndex(
+      (item) => item.ID === parseInt(data.appointment, 10)
+    );
+    if (index >= -1) {
+      setList2([...list2, list1[index]]);
+      const newList1 = [...list1];
+      newList1.splice(index, 1);
+      setList1(newList1);
+    }
+  };
 
-      if (index >= 0) {
-        var newList = JSON.parse(JSON.stringify(list));
-        newList.splice(index, 1);
-        setList(newList);
-
-        store.push([
-          {
-            type: "insert",
-            data: e.itemData,
-            index: e.toIndex
-          }
-        ]);
-      }
-    },
-    [list]
-  );
-
-  const onRemove = React.useCallback(
-    (e) => {
-      if (e.fromIndex >= 0) {
-        var newList = JSON.parse(JSON.stringify(list));
-        newList.push(e.itemData);
-        setList(newList);
-
-        store.push([
-          {
-            type: "remove",
-            key: e.itemData.ID
-          }
-        ]);
-      }
-    },
-    [list]
-  );
+  const onItemDrop = (data) => {
+    const index = list2.findIndex(
+      (item) => item.ID === parseInt(data.item, 10)
+    );
+    if (index >= -1) {
+      setList1([
+        ...list1,
+        {
+          ...list2[index],
+          Position: {
+            x: 20,
+            y: 20,
+          },
+        },
+      ]);
+      const newList2 = [...list2];
+      newList2.splice(index, 1);
+      setList2(newList2);
+    }
+  };
 
   return (
     <div
       style={{
-        display: "flex",
-        flexDirection: "row"
+        display: "grid",
+        gridTemplateColumns: "300px 1fr",
       }}
     >
-      <MyCanvas />
-      <DataGrid
-        id="dataGrid"
-        dataSource={store}
-        keyExpr="ID"
-        defaultColumns={columns}
-        showBorders={true}
-      >
-        <RowDragging
-          group={draggingGroupName}
-          onAdd={onAdd}
-          onRemove={onRemove}
-        />
-      </DataGrid>
+      <div>
+        <Droppable
+          types={["item"]}
+          onDrop={onItemDrop}
+          style={{
+            width: 300,
+            height: 300,
+          }}
+        >
+          <MyCanvas list={list1} />
+        </Droppable>
+      </div>
+      <div>
+        <Droppable types={["appointment"]} onDrop={onAppointmentDrop}>
+          <DraggableList list={list2} />
+        </Droppable>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+/*
+          <DataGrid
+          id="dataGrid"
+          dataSource={store}
+          keyExpr="ID"
+          defaultColumns={columns}
+          showBorders={true}
+        >
+          <RowDragging
+            group={draggingGroupName}
+            onAdd={onAdd}
+            onRemove={onRemove}
+            onDrop={onRowDrop}
+          />
+        </DataGrid> 
+*/
--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import "./styles.css";
-
 import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App.js";
 
-import themes from "devextreme/ui/themes";
-themes.initialized(() =>
-  ReactDOM.render(<App />, document.getElementById("app"))
-);
+import "./styles.css";
+
+ReactDOM.render(<App />, document.getElementById("app"));
